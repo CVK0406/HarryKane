b1afc771602fe52d3337a837607be7389c166300
Dile <duyle131103@gmail.com>
2023-05-29 23:17:36 +0700

    Làm phần Login Form

--- DoAnQLDA/src/doanqlda/DoAnQLDA.java
+++ DoAnQLDA/src/doanqlda/DoAnQLDA.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package doanqlda;
 
-/**
- *
- * @author Le Anh Tuan Dung
- */
+import GiaoDienNV.LoginForm;
+
+
 public class DoAnQLDA {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new LoginForm();
     }
-    
 }
